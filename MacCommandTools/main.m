487000102f014abf555fd6623640aaaae9b13510
Ramon Valdes <alevaldesochoa@gmail.com>
2020-03-14 16:59:51 +0100

    Initial Commit

---
+++ MacCommandTools/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MacCommandTools
+//
+//  Created by Ramon Valdes on 2020-03-14.
+//  Copyright © 2020 Ramon Valdes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AppleScriptObjC/AppleScriptObjC.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    [[NSBundle mainBundle] loadAppleScriptObjectiveCScripts];
+    return NSApplicationMain(argc, argv);
+}
